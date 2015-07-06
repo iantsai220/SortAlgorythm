743722dd02011909910646bf52ba342bfe8726d9
iantsai <iantsai86@gmail.com>
2015-07-05 22:05:27 -0700

    finished bubble sort

--- SortAlgorythm/ViewController.m
+++ SortAlgorythm/ViewController.m
@@ -17,11 +17,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *dArray =@[@101, @201, @301, @121,@11,@123,@21,@14,@32,@76,@89,@987,@65];
+    NSMutableArray *unsortedArray = [NSMutableArray arrayWithArray:dArray];
+    
+    NSLog(@"%@", unsortedArray);
+    
+    NSArray *sortedArray = [self bubbleSort:unsortedArray];
+    
+    NSLog(@"%@", sortedArray);
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSArray *)bubbleSort:(NSMutableArray *)unsortedArray {
+    
+    BOOL swapped = YES;
+    
+    while (swapped) {
+        
+        swapped = NO;
+    
+        for (int i = 1; i < unsortedArray.count; i++) {
+            
+            if (unsortedArray[i-1] > unsortedArray[i]) {
+                
+                [unsortedArray exchangeObjectAtIndex:i-1 withObjectAtIndex:i];
+                
+                swapped = YES;
+            }
+        }
+    }
+    
+    
+    
+    return unsortedArray;
 }
 
 @end
