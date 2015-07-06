b68a8124957369514984f613b44731931c2f1086
iantsai <iantsai86@gmail.com>
2015-07-05 18:22:30 -0700

    Initial Commit

---
+++ SortAlgorythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SortAlgorythm
+//
+//  Created by Ian Tsai on 2015-07-05.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SortAlgorythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SortAlgorythm
+//
+//  Created by Ian Tsai on 2015-07-05.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
